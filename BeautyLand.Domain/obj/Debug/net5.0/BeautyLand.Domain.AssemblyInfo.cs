e1c29e8ab1223aaf7923d7c664512fc9de4232ba
YasaminZamanzadeh <zamanzadehyasamin@yahoo.com>
2024-11-04 12:12:58 +0100

    Some data that was not related to a specific layer is added

--- BeautyLand.Domain/obj/Debug/net5.0/BeautyLand.Domain.AssemblyInfo.cs
+++ BeautyLand.Domain/obj/Debug/net5.0/BeautyLand.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeautyLand.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c3e4e48ef4f004f8c5e0f26dfecea0ce028ccc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeautyLand.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeautyLand.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
